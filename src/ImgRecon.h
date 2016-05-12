e31d3fd959ccc18392451726d192b3663734358a
Mozhi <mozhi@mozhi-K45VD.(none)>
2016-05-12 09:10:14 +0800

    fix TAB problem.

--- src/ImgRecon.h
+++ src/ImgRecon.h
@@ -15,28 +15,28 @@
 using namespace std;
 class ImgRecon  
 {
-public:
-	ImgRecon(IplImage *img);//可以直接传彩图
-	virtual ~ImgRecon();
-	void ReInit(IplImage *img);//更改下一张图片
-	int GetNumber();//获得图片中的数字，-2未处理，-1找不到
-	CvPoint GetCenterPoint();//获得轮廓中心
-	int GetContourArea();//获取轮廓面积
-	bool ContourExist();//轮廓存在与否
+  public:
+    ImgRecon(IplImage *img);//可以直接传彩图
+    virtual ~ImgRecon();
+    void ReInit(IplImage *img);//更改下一张图片
+    int GetNumber();//获得图片中的数字，-2未处理，-1找不到
+    CvPoint GetCenterPoint();//获得轮廓中心
+    int GetContourArea();//获取轮廓面积
+    bool ContourExist();//轮廓存在与否
 
-private:
-	int NumResult;//判断所得数字
-	bool ConExist;//轮廓是否存在
-	int ConArea;//轮廓面积
-	CvPoint Center;//轮廓中心点
-	CvPoint2D32f corners1[4];//四个顺时针排列的角点，第一个为左上
-	string SamLoca[10];//样本位置
-	int NumDetec(const IplImage* dst);//数字判断函数
-	IplImage 
-		*src;//灰度图，二值化，腐蚀
-	//	*dst;//复制src后找轮廓，存放标准化后的图片
-	//	*dst1;//画拟合曲线，找角点
-	
+  private:
+    int NumResult;//判断所得数字
+    bool ConExist;//轮廓是否存在
+    int ConArea;//轮廓面积
+    CvPoint Center;//轮廓中心点
+    CvPoint2D32f corners1[4];//四个顺时针排列的角点，第一个为左上
+    string SamLoca[10];//样本位置
+    int NumDetec(const IplImage* dst);//数字判断函数
+    IplImage 
+      *src;//灰度图，二值化，腐蚀
+    //	*dst;//复制src后找轮廓，存放标准化后的图片
+    //	*dst1;//画拟合曲线，找角点
+
 };
 
 #endif // !defined(AFX_IMGRECON_H__A74914C1_139A_4D1D_B134_668BD20B3AF8__INCLUDED_)
